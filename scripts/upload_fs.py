82a2409e7027c8f22ec7793ee4eedaea143053f1
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2025-09-07 09:16:08 +0700

    Update upload_fs.py

--- scripts/upload_fs.py
+++ scripts/upload_fs.py
@@ -1,16 +1,20 @@
 Import("env")
 import os
+import shutil
 
-# path data di dalam library (otomatis dicari di .pio/libdeps)
 lib_data_dir = os.path.join(env['PROJECT_LIBDEPS_DIR'], env['PIOENV'], "espServer", "data")
+project_data_dir = os.path.join(env['PROJECT_DIR'], "data")
 
 def before_upload(source, target, env):
-    if os.path.isdir(lib_data_dir):
-        print(">>> Using library data folder:", lib_data_dir)
-        env.Replace(DATA_DIR=lib_data_dir)
-    else:
-        print(">>> No data folder found in library")
+    # Hapus folder data project lama jika ada
+    if os.path.isdir(project_data_dir):
+        shutil.rmtree(project_data_dir)
+    # Copy folder data library ke project
+    shutil.copytree(lib_data_dir, project_data_dir)
+    env.Replace(DATA_DIR=project_data_dir)
+    print(">>> Using library data folder:", project_data_dir)
 
-# Pastikan filesystem image dibangun dulu sebelum upload firmware
+# Jalankan sebelum upload firmware
 env.AddPreAction("upload", before_upload)
+# Pastikan uploadfs dibangun dulu
 env.Depends("upload", "uploadfs")
