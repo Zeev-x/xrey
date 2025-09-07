73ac11d46d7d3fe00dabdb885f22bf9c6d8a3803
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2025-09-07 10:50:46 +0700

    Update upload_fs.py

--- scripts/upload_fs.py
+++ scripts/upload_fs.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 lib_data_dir = os.path.join(env['PROJECT_LIBDEPS_DIR'], env['PIOENV'], "espServer", "data")
-project_data_dir = os.path.join(env['PROJECT_DIR'], "data_temp")
+project_data_dir = os.path.join(env['PROJECT_DIR'], "data")
 
 def before_upload(source, target, env):
     if os.path.isdir(project_data_dir):
@@ -19,3 +19,4 @@ def before_upload(source, target, env):
     print(">>> Temporary data folder removed.")
 
 env.AddPreAction("upload", before_upload)
+
